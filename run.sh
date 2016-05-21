7770ea3991aab27d01ffbdfcc5ba236b8363bec4
Rex Tsai (蔡志展) <rex.cc.tsai@gmail.com>
2016-05-21 23:52:48 +0800

    Take docker instance from parameter.

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-POSTGREDB_DOCKER=osm-postgis
+POSTGREDB_DOCKER=${1:-"osm-postgis"}
 docker run --rm -t -i --link ${POSTGREDB_DOCKER}:pg -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY osmtw/pgadmin3
